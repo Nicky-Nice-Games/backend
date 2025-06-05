5afea39b644684bfc53014d46cdc28b1989ee675
GrimmKinNovice <77074039+GrimmKinNovice@users.noreply.github.com>
2025-06-05 14:35:40 -0400

    Check

---
+++ src/main/java/RITIGM/gokartproject/LambdaHandler.java
@@ -0,0 +1,16 @@
+package RITIGM.gokartproject;
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class); }
+        catch (ContainerInitializationException ex){
+            throw new RuntimeException("Unable to load spring boot application",ex); }
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
+    }
+}
